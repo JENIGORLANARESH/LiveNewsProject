2fce663e3a5205e24944bbd39eba42a3a94757d3
JENIGORLA NARESH <nareshjenigorla@gmail.com>
2023-08-18 12:06:48 +0530

    modified app.js

--- app.js
+++ app.js
@@ -13,66 +13,14 @@ const yourBearerToken = "5e102aefdf8d47f3943f0f53c2f935fa";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", async (req, res) => {
- 
-  try {
-    const result = await axios.get(
-      API_URL + `world&sortBy=popularity&apiKey=${yourBearerToken}`
-    );
-   
-    const page = parseInt(req.query.page) || 1;
-    const articlesPerPage = 16;
-    const totalArticles = result.data.totalResults;
-   
-    const totalPages = Math.ceil(totalArticles / articlesPerPage);
-    const startIndex = (page - 1) * articlesPerPage;
-    const endIndex = startIndex + articlesPerPage;
-
-    const articlesToDisplay = result.data.articles.slice(startIndex, endIndex);
-
-    res.render("index.ejs", {
-      totalArticles: totalArticles,
-      articles: articlesToDisplay,
-      total: "Headlines",
-      currentPage: page,
-      totalPages: totalPages,
-    });
-  } catch (error) {
-    res.render("index.ejs", {
-      error: error.message,
-    });
-  }
+app.get("/", (req,res) => {
+  const homepage = "TopNews";
+  res.redirect(`/${homepage}`);
 });
 
-app.post("/search", async (req, res) => {
+app.post("/search", (req, res) => {
   const requestedTitle = req.body["category"];
-  try {
-    const result = await axios.get(
-      API_URL + requestedTitle + `&sortBy=popularity&apiKey=${yourBearerToken}`
-    );
-
-    const page = parseInt(req.query.page) || 1; 
-    const articlesPerPage = 16;
-
-    const totalArticles = result.data.totalResults;
-    const totalPages = Math.ceil(totalArticles / articlesPerPage);
-    const startIndex = (page - 1) * articlesPerPage;
-    const endIndex = startIndex + articlesPerPage;
-
-    const articlesToDisplay = result.data.articles.slice(startIndex, endIndex);
-
-    res.render("index.ejs", {
-      totalArticles: totalArticles,
-      articles: articlesToDisplay,
-      total: requestedTitle,
-      currentPage: page,
-      totalPages: totalPages,
-    });
-  } catch (error) {
-    res.render("index.ejs", {
-      error: error.message,
-    });
-  }
+  res.redirect(`/${requestedTitle}`);
 });
 
 app.get("/:postName", async (req, res) => {
